2585227b6af997f6b04a07044e23800ef838d3b4
SoniaRoss <srross99@gmail.com>
2022-03-03 19:01:37 -0500

    update lab

---
+++ lab_MOVIE_DATABASE/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_MOVIE_DATABASE
+{
+    public class Movie
+    {
+        private string _category; //get/set not needed when private
+        private string _title;
+
+        public Movie(string category, string title) 
+        {
+            this._category = category;  
+            this._title = title;
+        }
+
+        public string GetCategory()
+        {
+           return _category;
+        }
+
+        public string GetTitle()
+        {
+            return _title;
+        }
+
+    }
+}
--- lab_MOVIE_DATABASE/Program.cs
+++ lab_MOVIE_DATABASE/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using lab_MOVIE_DATABASE;
+
+List<Movie> movieList = new List<Movie>();
+{
+    new Movie("Action", "Black Panther");
+    new Movie("Comedy", "Madea Homecoming");
+    new Movie("Sci-Fi", "The Matrix Reloaded");
+    new Movie("Action", "Avatar");
+    new Movie("Adventure", "Black Panther");
+    new Movie("Adventure", "The Lion King");
+    new Movie("Action", "Moana");
+    new Movie("Drama", "Mahogany");
+    new Movie("Comedy", "he Water Boy");
+    new Movie("Action", "Black Panther");
+}
+
+Console.WriteLine("Entr a category: action comedy sci-fi adventure drama");
+
+var userInput = Console.ReadLine();
+
+List<Movie> selectedList = movieList.Where(x => x.GetCategory() == userInput).ToList());
+
+foreach(Movie movie in selectedList)
+{
+    Console.WriteLine(movie.GetTitle);
+}
